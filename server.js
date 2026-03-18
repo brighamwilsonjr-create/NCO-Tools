76481b4db9cffaa1642a84fd2d7c4ba394fe6bb5
brighamwilsonjr-create <brighamwilsonjr@gmail.com>
2026-03-18 17:32:27 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const PDFDocument = require('pdfkit');
 const { calculateAFT } = require('./aft_tables');
 
 
